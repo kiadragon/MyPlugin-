859a2640c375373b6cc5b413e3cf21054158dbda
kiadragon <sxhdragon@gmail.com>
2014-04-25 17:51:56 +0800

    demoUpload

--- 修改字体/fontDealing.py
+++ 修改字体/fontDealing.py
@@ -4,7 +4,8 @@ import re
 
 Fontfile = open('font/font.txt', 'r')
 font = Fontfile.read()
-print "Please input letter you want to match"
-p = re.compile('[c]+')
+keyboradInput = ''
+# keyboradInput = input("please tell me what you want to find:")
+p = re.compile('\w*[a]\w*')
 m = p.findall(font)
-print m
+print(m)
